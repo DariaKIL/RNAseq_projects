ced4d0b068e7e2de1bb350cad3df367d95b0a12b
DariaKIL <dasha-kilina@mail.ru>
2024-08-12 20:20:30 +0300

    Add results

---
+++ Wound_Signaling_Arabidopsis/data/download_convertSRR.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# List of SRR IDs
+samples=(
+"SRR12649400"
+"SRR12649401"
+"SRR12649399"
+"SRR12649403"
+"SRR12649402"
+"SRR12649404")
+
+# Downloading using prefetch и converting to FASTQ using fastq-dump
+for sample in "${samples[@]}"
+do
+    echo "Downloading $sample"
+    if prefetch "$sample"; then
+        echo "Successfully downloaded $sample"
+    else
+        echo "Failed to download $sample" >&2
+        continue
+    fi
+    
+    echo "Converting $sample to FASTQ.gz"
+    if fastq-dump --split-files --gzip "$sample"; then
+        echo "Successfully converted $sample to FASTQ"
+    else
+        echo "Failed to convert $sample to FASTQ.gz" >&2
+        continue
+    fi
+done
+
+echo "All samples processed"
